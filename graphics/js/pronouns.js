94e9575396c4a94f82cf30b94d0f197e0b28c83f
BashPrime <5769306+BashPrime@users.noreply.github.com>
2021-02-12 19:52:42 -0800

    Update pronouns

--- graphics/js/pronouns.js
+++ graphics/js/pronouns.js
@@ -5,7 +5,26 @@ const SHE_THEY = 'she/they';
 const THEY_THEM = 'they/them';
 
 const pronouns = {
-	nes: HE_HIM
+	nes: HE_THEY,
+	GhostKumo: HE_HIM,
+	Jaxler: HE_HIM,
+	FromDarkHell: HE_HIM,
+	KonceptioN2: HE_HIM,
+	MrCab55: HE_HIM,
+	KLM1187: HE_HIM,
+	Torpedo: HE_HIM,
+	DemonicRobots: HE_HIM,
+	Aggy: THEY_THEM,
+	Seckswrecks: THEY_THEM,
+	PeekingBoo: HE_HIM,
+	kirbymastah: HE_HIM,
+	Kitt: THEY_THEM,
+	Jai_Heart: HE_HIM,
+	livelyraccoon: HE_HIM,
+	amber_cxc: SHE_HER,
+	Mr_Shasta: HE_HIM,
+	Crispyspeedruns: HE_HIM,
+	'The Sound Defense': HE_HIM
 };
 
 function getPronounsForPlayer(playerName) {
